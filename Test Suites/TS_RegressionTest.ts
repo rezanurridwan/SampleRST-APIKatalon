9791a104479accde8b0ca36ad7f9b45e6c195200
LENOVO <LENOVO@DESKTOP-0EH6HA0>
2023-05-24 07:20:08 +0700

    create TC01_List_User

---
+++ Test Suites/TS_RegressionTest.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_RegressionTest</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>2572ccbe-e954-4f02-bf3a-34b88501a2b3</testSuiteGuid>
+</TestSuiteEntity>
